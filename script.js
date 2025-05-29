dae621e83fad08ad3d095f86ed6e084c57b58d43
Azkaahps <azkasepto44@gmail.com>
2025-05-29 11:25:47 +0700

    fix eror js

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
 // Toggle class active
-const navbarNav = document.querySelector ('.navbar');
+const navbarNav = document.querySelector('.navbar');
 
-
-// ==============navbar hide and show==============
-
+// Navbar hide and show
 const barbtn = document.getElementById("bar-icon");
 const dropdown = document.getElementById("menu");
 
-barbtn.addEventListener("click", ()=>{
+barbtn.addEventListener("click", () => {
     dropdown.classList.toggle("show");
-})
+});
 
-
-// submenu
-
+// Submenu toggle
 const droptog = document.querySelectorAll(".droptoggle");
 const submenu = document.getElementsByClassName("submenu");
 
-for(let x = 0; x < droptog.length; x++){
-    droptog[x].addEventListener("click", ()=>{
+for (let x = 0; x < droptog.length; x++) {
+    droptog[x].addEventListener("click", () => {
         submenu[x].classList.toggle("submenushow");
-    })
+    });
+}
 
 // Klik diluar sidebar untuk menghilangkan nav
-const barbtn = document.querySelector('bar-icon')
-
-document.addEventListener('click', function(e){
-    if(!barbtn.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove('active');
+document.addEventListener("click", function(e) {
+    if (!barbtn.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove("active");
     }
-}
+});
